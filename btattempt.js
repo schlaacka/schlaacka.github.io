aa2b3dcb4199b1e5e04b496f54969cef4aa03b65
schlaacka <145715641+schlaacka@users.noreply.github.com>
2024-02-28 13:47:57 -0600

    Update btattempt.js

--- btattempt.js
+++ btattempt.js
@@ -132,8 +132,7 @@ window.loadLeverJobs = function (options) {
           content += '<p class="lever-job" data-department="' + groupedPostings[i].departmentTitle +'" data-team="' + groupedPostings[i].teams[j].postings[k].categories.team + '" data-location="' + groupedPostings[i].teams[j].postings[k].categories.location + '"data-work-type="' + groupedPostings[i].teams[j].postings[k].categories.commitment + '">' +
           '<a class="lever-job-title" href="' + groupedPostings[i].teams[j].postings[k].hostedUrl + '">' +
           sanitizeForHTML(groupedPostings[i].teams[j].postings[k].text) + '<a><span class="lever-job-tag">' + '</br>' +
-          '<p class="lever-team-title">' +
-          sanitizeForHTML(groupedPostings[i].teams[j].teamTitle) || '') + '&nbsp;&#8212;&nbsp;' + (sanitizeForHTML(groupedPostings[i].teams[j].postings[k].categories.location) || '') + '</br>';
+          '<p>' + sanitizeForHTML(groupedPostings[i].teams[j].teamTitle) || '') + '&nbsp;&#8212;&nbsp;' + (sanitizeForHTML(groupedPostings[i].teams[j].postings[k].categories.location) || '') + '</br>';
         }
 
         content += '</ul>';
