aee85512bd608c33fff6963605a07880a3f8d541
schlaacka <145715641+schlaacka@users.noreply.github.com>
2024-02-28 13:57:43 -0600

    Update btattempt.js

--- btattempt.js
+++ btattempt.js
@@ -129,10 +129,16 @@ window.loadLeverJobs = function (options) {
         //content += '<ul class="lever-team" data-team="' + groupedPostings[i].teams[j].teamTitle + '"><h4 class="lever-team-title">' + sanitizeForHTML(groupedPostings[i].teams[j].teamTitle) + '</h4>';
 
         for (var k = 0; k < groupedPostings[i].teams[j].postings.length; k ++) {
-          content += '<p class="lever-job" data-department="' + groupedPostings[i].departmentTitle +'" data-team="' + groupedPostings[i].teams[j].postings[k].categories.team + '" data-location="' + groupedPostings[i].teams[j].postings[k].categories.location + '"data-work-type="' + groupedPostings[i].teams[j].postings[k].categories.commitment + '">' +
-          '<a class="lever-job-title" href="' + groupedPostings[i].teams[j].postings[k].hostedUrl + '">' +
-          sanitizeForHTML(groupedPostings[i].teams[j].postings[k].text) + '<a><span class="lever-job-tag">' + '</br>' +
-          '<p>' + sanitizeForHTML(groupedPostings[i].teams[j].teamTitle) || '') + '&nbsp;&#8212;&nbsp;' + (sanitizeForHTML(groupedPostings[i].teams[j].postings[k].categories.location) || '') + '</br>';
+          content += 
+            '<p class="lever-job" data-department="' + groupedPostings[i].departmentTitle + '
+            " data-team="' + groupedPostings[i].teams[j].postings[k].categories.team + '
+            " data-location="' + groupedPostings[i].teams[j].postings[k].categories.location + '
+            " data-work-type="' + groupedPostings[i].teams[j].postings[k].categories.commitment + '">
+            ' +
+
+            '<a class="lever-job-title" href="' + groupedPostings[i].teams[j].postings[k].hostedUrl + '">' +
+            sanitizeForHTML(groupedPostings[i].teams[j].postings[k].text) + '<a><span class="lever-job-tag">' + '</br>' +
+            sanitizeForHTML(groupedPostings[i].teams[j].teamTitle) || '') + <p> + '&nbsp;&#8212;&nbsp;' + (sanitizeForHTML(groupedPostings[i].teams[j].postings[k].categories.location) || '') + '</br>';
         }
 
         content += '</ul>';
