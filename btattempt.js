265cc400c900a867cb5977699c7edd5b452c414e
schlaacka <145715641+schlaacka@users.noreply.github.com>
2024-02-28 15:09:07 -0600

    Update btattempt.js

--- btattempt.js
+++ btattempt.js
@@ -1,12 +1,12 @@
-window.loadLeverJobs = function (options) {
-
-  .standard-font-size {
+.standard-font-size {
   font-size: 1rem;
 }
 
 .bold-job-title {
   font-weight: bold;
 }
+
+window.loadLeverJobs = function (options) {
 
   //Checking for potential Lever source or origin parameters
   var pageUrl = window.location.href;
